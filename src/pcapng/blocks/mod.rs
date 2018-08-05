0f70426c1ea56c80096428dc47e34c03eed30520
Flier Lu <flier.lu@gmail.com>
2018-08-06 00:27:28 +0800

    export options code

--- src/pcapng/blocks/mod.rs
+++ src/pcapng/blocks/mod.rs
@@ -1,3 +1,5 @@
 mod section_header;
 
-pub use self::section_header::{shb_hardware, shb_os, shb_userappl, SectionHeader};
+pub use self::section_header::{
+    shb_hardware, shb_os, shb_userappl, SectionHeader, SHB_HARDWARE, SHB_OS, SHB_USERAPPL,
+};
--- src/pcapng/mod.rs
+++ src/pcapng/mod.rs
@@ -3,7 +3,7 @@ mod blocks;
 mod options;
 
 pub use self::block::Block;
-pub use self::blocks::{shb_hardware, shb_os, shb_userappl, SectionHeader};
+pub use self::blocks::*;
 pub use self::options::{
     comment, custom_bytes, custom_private_bytes, custom_private_str, custom_str, end_of_opt, opt,
     Opt, Options,
